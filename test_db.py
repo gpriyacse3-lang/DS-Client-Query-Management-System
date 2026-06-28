fa1a96ce428a82465785cd6f257451ae6a9cfe57
IMDB-movie-recommendations-main <gpriyacse3@gmail.com>
2026-06-28 22:20:27 +0530

    Delete test_db.py

--- test_db.py
+++
@@ -1,8 +0,0 @@
-from db_connection import get_connection
-
-try:
-    conn = get_connection()
-    print("Database connected successfully")
-    conn.close()
-except Exception as e:
-    print("Connection failed:", e)
